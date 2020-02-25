fcea3f49860bfd59a668f2173fde5975e25def8d
MomoCow <momocow.me@gmail.com>
2020-02-25 23:47:41 +0800

    Improve demo.

--- demo/basic/main.py
+++ demo/basic/main.py
@@ -1,31 +1,45 @@
+import asyncio
 import time
 
 from aioeventbus import EventBus, Listener
-
 from events import LifecycleEvent, ShutdownEvent, StartupEvent
 
 listener_1 = Listener()
 listener_2 = Listener()
 
+events = []
+
+
 @listener_1.on(LifecycleEvent)
 async def on_lifecycle(event):
-    print("Lifecycle event:", event.time)
+    print("Lifecycle event:", event.time, f"({time.time() - event.time})")
+    events.append(event)
+
 
 @listener_2.on(StartupEvent)
 async def on_startup(event):
-    print(event)
+    await asyncio.sleep(2)
+    print(event, f"({time.time() - event.time})")
+
 
 @listener_2.on(ShutdownEvent)
 async def on_shutdown(event):
-    print(event)
+    print(event, f"({time.time() - event.time})")
+    await asyncio.sleep(2)
+
 
 async def main():
     bus = EventBus()
     bus.register(listener_1)
     bus.register(listener_2)
-    await bus.emit_parallel(StartupEvent())
-    await bus.emit_parallel(ShutdownEvent())
+
+    startup_event = StartupEvent()
+    await bus.emit_parallel(startup_event)
+
+    shutdown_event = ShutdownEvent()
+    await bus.emit_series(shutdown_event)
+
+    assert events == [startup_event, shutdown_event]
 
 if __name__ == "__main__":
-    import asyncio
     asyncio.run(main())
