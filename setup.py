b842ccbafbbf7b736470e907b50919b0057bf5f1
MomoCow <taocheng@qnap.com>
2020-02-25 20:39:58 +0800

    Set up workspace.

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from distutils.core import setup
+
+NAME = "aioeventbus"
+
+with open("README.md", "r", encoding="utf-8") as f:
+    LONG_DESC = f.read()
+
+here = os.path.dirname(__file__)
+ABOUT = {}
+
+with open(os.path.join(here, NAME, '__version__.py')) as f:
+    exec(f.read(), ABOUT)
+
+setup(
+    name=NAME,
+    description="simple in-process event bus system",
+    long_description=LONG_DESC,
+    long_description_content_type="text/markdown",
+    version=ABOUT["__version__"],
+    author="momocow",
+    author_email="momocow.me@gmail.com",
+    license="MIT",
+    url="https://github.com/momocow/python-aioeventbus",
+    packages=["aioeventbus"],
+    classifiers=[
+        "Framework :: AsyncIO",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5"
+    ]
+)
