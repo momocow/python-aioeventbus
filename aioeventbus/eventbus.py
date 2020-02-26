b8423620b1b3926ad44c719e5521b237869e6a0d
MomoCow <taocheng@qnap.com>
2020-02-26 18:32:16 +0800

    Make EventBus pipable.

--- aioeventbus/eventbus.py
+++ aioeventbus/eventbus.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import asyncio
 from contextlib import contextmanager
 from typing import List, Optional, Tuple, Type
@@ -12,6 +14,7 @@ from .typing import EventBase, EventClass, Handler
 class EventBus:
     def __init__(self):
         self.__listeners: List[Listener] = []
+        self.__children: List[EventBus] = []
 
     def register(self, *listeners: Listener):
         self.__listeners.extend(listeners)
@@ -23,19 +26,23 @@ class EventBus:
     def clear(self):
         return self.__listeners.clear()
 
-    def _get_events(self, event: EventBase) -> Tuple[EventClass]:
+    def get_propagation_order(self, event: EventBase) -> Tuple[EventClass]:
         return event.__class__.__mro__[:-1]  # exclude object class
 
-    def _get_handlers(self, event_cls: EventClass) -> Tuple[Handler]:
+    def get_handlers(self, event_cls: EventClass) -> Tuple[Handler]:
         return (h
                 for l in self.__listeners
                 for h in l.get(event_cls, []))
 
     @contextmanager
     def __event_context(self, event: EventBase):
-        token = event_v.set(event)
+        token = None
+        prev_event = event_v.get(None)
+        if prev_event is not event:
+            token = event_v.set(event)
         yield
-        event_v.reset(token)
+        if token is not None:
+            event_v.reset(token)
 
     async def __call_handler(self,
                              handler: Handler,
@@ -59,13 +66,16 @@ class EventBus:
             raise TypeError("\"event\" should be an instance of Event.")
 
         with self.__event_context(event):
-            for e in self._get_events(event):
-                for h in self._get_handlers(e):
+            for e in self.get_propagation_order(event):
+                for h in self.get_handlers(e):
                     try:
                         await self.__call_handler(h, event,
                                                   return_exceptions=False)
                     except (StopIteration, StopAsyncIteration):
                         return
+
+            for c in self.__children:
+                await c.emit_series(event)
 
     async def emit_parallel(self, event: EventBase, *,
                             return_exceptions: bool = False
@@ -74,15 +84,36 @@ class EventBus:
             raise TypeError("\"event\" should be an instance of Event.")
 
         with self.__event_context(event):
-            coros = (
+            coros = list(
                 self.__call_handler(h, event,
                                     return_exceptions=return_exceptions)
-                for e in self._get_events(event)
-                for h in self._get_handlers(e)
+                for e in self.get_propagation_order(event)
+                for h in self.get_handlers(e)
             )
-            returns = await asyncio.gather(*coros)
+            coros_len = len(coros)
+            child_coros = (
+                c.emit_parallel(event,
+                                return_exceptions=return_exceptions)
+                for c in self.__children
+            )
+            returns = await asyncio.gather(*coros, *child_coros,
+                                           return_exceptions=return_exceptions)
 
-        return (r for r in returns if isinstance(r, HandlerError))
+        return tuple(r for r in returns[:coros_len]
+                     if isinstance(r, HandlerError)) + \
+            tuple(r for rs in returns[coros_len:] for r in rs)
+
+    def pipe(self, bus: EventBus):
+        self.__children.append(bus)
+
+    def unpipe(self, bus: EventBus):
+        self.__children.remove(bus)
+
+    def attach(self, bus: EventBus):
+        bus.pipe(self)
+
+    def detach(self, bus: EventBus):
+        bus.unpipe(self)
 
     @property
     def events(self):
