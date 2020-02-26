4dde4df08116c35b5a9bc0d757dfd92acf2e933e
MomoCow <taocheng@qnap.com>
2020-02-26 09:54:02 +0800

    Update to support python3.7+ only.

--- setup.py
+++ setup.py
@@ -27,6 +27,6 @@ setup(
         "Framework :: AsyncIO",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5"
+        "Programming Language :: Python :: 3.7"
     ]
 )
