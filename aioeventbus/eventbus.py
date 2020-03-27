8b9ca38e6934cc7c191fce52a1184e5024a72491
momocow <momocow.me@gmail.com>
2020-03-27 16:50:32 +0800

    Add emit_sync.

--- aioeventbus/eventbus.py
+++ aioeventbus/eventbus.py
@@ -51,6 +51,30 @@ class EventBus:
                 for l in (self.__default_listener, *self.__listeners)
                 for h in l.get(event_cls, []))
 
+    def __on_handler_error(self,
+                           handler: Handler,
+                           event: EventBase,
+                           exc: Exception, *,
+                           return_exceptions: bool = False):
+        if return_exceptions:
+            try:
+                raise HandlerError(handler, event) from exc
+            except HandlerError as exc:
+                return exc
+        else:
+            raise HandlerError(handler, event) from exc
+
+    def __call_sync_handler(self,
+                            handler: Handler,
+                            event: EventBase, *,
+                            return_exceptions: bool = False
+                            ) -> Optional[HandlerError]:
+        try:
+            # create handler context
+            ret = handler(event)
+        except Exception as exc:
+            return self.__on_handler_error(handler, event, exc)
+
     async def __call_handler(self,
                              handler: Handler,
                              event: EventBase, *,
@@ -62,13 +86,7 @@ class EventBus:
             if asyncio.iscoroutine(ret):
                 await asyncio.create_task(ret)
         except Exception as exc:
-            if return_exceptions:
-                try:
-                    raise HandlerError(handler, event) from exc
-                except HandlerError as exc:
-                    return exc
-            else:
-                raise HandlerError(handler, event) from exc
+            return self.__on_handler_error(handler, event, exc)
 
     async def emit_series(self, event: EventBase, *, propagate: bool = True):
         if not isinstance(event, Event):
@@ -113,6 +131,22 @@ class EventBus:
         return tuple(r for r in returns[:coros_len]
                      if isinstance(r, HandlerError)) + \
             tuple(r for rs in returns[coros_len:] for r in rs)
+
+    def emit_sync(self, event: EventBase, *, propagate: bool = True):
+        if not isinstance(event, Event):
+            raise TypeError("\"event\" should be an instance of Event.")
+
+        for e in self.get_propagation_order(event):
+            for h in self.get_handlers(e):
+                try:
+                    self.__call_sync_handler(h, event,
+                                             return_exceptions=False)
+                except (StopIteration, StopAsyncIteration):
+                    return
+
+        if propagate:
+            for c in self.__children:
+                c.emit_sync(event)
 
     def pipe(self, bus: EventBus):
         self.__children.append(bus)
